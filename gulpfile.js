48d716ed03d0ca9e9c4b25e9f13eb9be7da0e574
Stephen O'Kennedy <stephenoken@gmail.com>
2015-12-28 19:57:45 +0000

    "Fleshed out gulp"
    ""

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var gzip = require('gulp-gzip');
 
 gulp.task("styles",function () {
   return gulp.src("./src/styles/**/*.scss")
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(sourcemaps.write('./maps'))
+    .pipe(sourcemaps.init())
+    .pipe(concat("styles.css"))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gzip())
     .pipe(gulp.dest("./public/"))
 });
 

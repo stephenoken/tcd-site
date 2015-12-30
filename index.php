82a852f2673c11dec4483d600f3d047294da8de9
Stephen O'Kennedy <stephenoken@gmail.com>
2015-12-30 23:30:00 +0000

    "Added more charts"
    ""

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
           </div>
           <div class="mdl-layout-spacer"></div>
           <nav class="mdl-navigation mdl-layout--large-screen-only">
-            <a href="tel:353851410017" class="mdl-navigation__link"><i class="material-icons material-text">phone</i>0851410017</a>
+            <a href="tel:012826156" class="mdl-navigation__link"><i class="material-icons material-text">phone</i>0851410017</a>
             <a href="mailto:okenneds@tcd.ie" class="mdl-navigation__link"><i class="material-icons material-text">mail</i>okenneds@tcd.ie</a>
           </nav>
         </div>
@@ -27,13 +27,13 @@
       <div class="mdl-layout__drawer">
         <span class="mdl-layout-title">Stephen O'Kennedy</span>
         <nav class="mdl-navigation">
-          <a href="tel:353851410017" class="mdl-navigation__link"><i class="material-icons material-text">phone</i>0851410017</a>
+          <a href="tel:0851410017" class="mdl-navigation__link"><i class="material-icons material-text">phone</i>0851410017</a>
           <a href="mailto:okenneds@tcd.ie" class="mdl-navigation__link"><i class="material-icons material-text">mail</i>okenneds@tcd.ie</a>
         </nav>
       </div>
       <main class="mdl-layout__content">
       <div class="mdl-grid ">
-        <div class="mdl-card mdl-cell mdl-cell--6-col mdl-cell--12-col-phone mdl-shadow--2dp">
+        <!-- <div class="mdl-card mdl-cell mdl-cell--6-col mdl-cell--12-col-phone mdl-shadow--2dp">
           <div class="mdl-card__title">
             <h2 class="mdl-card__title-text">Work Experience</h2>
           </div>
@@ -77,25 +77,98 @@
           <div class="mdl-card__menu">
             <i class="material-icons">code</i>
           </div>
-        </div>
+        </div> -->
         <!-- <h4>Standby... I'm going to create my own personal blog below.</h4> -->
-        <div class="mdl-card mdl-cell container mdl-shadow--2dp">
+        <div class="mdl-card mdl-cell mdl-cell--6-col mdl-cell--12-col-phone mdl-shadow--2dp">
+          <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">Distance & Times</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            <table id="running_table" class="mdl-data-table mdl-js-data-table">
+              <tr>
+                <th class="mdl-data-table__cell--non-numeric">
+                  Distance (km)
+                </th>
+                <th class="mdl-data-table__cell--non-numeric">
+                  Time
+                </th>
+                <th class="mdl-data-table__cell--non-numeric">
+                  Date
+                </th>
+                <th class="mdl-data-table__cell--non-numeric">
+                  Average Cadence
+                </th>
+                <th class="mdl-data-table__cell--non-numeric">
+                  Average HR
+                </th>
+              </tr>
+            </table>
+          </div>
+          <div class="mdl-card__menu">
+            <i class="material-icons">directions_run</i>
+          </div>
+        </div>
+        <div class="mdl-card mdl-cell mdl-cell--6-col mdl-cell--12-col-phone mdl-shadow--2dp">
+          <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">Goals for the year</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            <p>
+              <i class="material-icons">crop_square</i> I'll think of something
+            </p>
+          </div>
+          <div class="mdl-card__menu">
+            <i class="material-icons">done_all</i>
+          </div>
+        </div>
+        <div class="mdl-card mdl-cell  mdl-cell--12-col container mdl-shadow--2dp">
             <div class="mdl-card__title">
-              <h2 class="mdl-card__title-text">Heart rate from the past month</h2>
+              <h2 class="mdl-card__title-text">Heart Rate</h2>
             </div>
             <canvas id="heart_rate_chart"></canvas>
+            <div id="heart_rate_chart_placeholder"></div>
+            <div class="mdl-card__menu">
+              <i class="material-icons">favorite</i>
+            </div>
         </div>
-        <div class="mdl-card mdl-cell container mdl-shadow--2dp">
+        <div class="mdl-card mdl-cell mdl-cell--4-col mdl-cell--12-col-phone mdl-shadow--2dp">
+          <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">Exercise Types</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            <canvas id="exercise_chart"></canvas>
+            <div id="exercise_chart_placeholder"></div>
+          </div>
+          <div class="mdl-card__menu">
+            <i class="material-icons">accessibility</i>
+          </div>
+        </div>
+        <div class="mdl-card mdl-cell mdl-cell--8-col mdl-cell--12-col-phone mdl-shadow--2dp">
+          <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">Upcoming Races</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            <p>
+              National Intermediates 24th of Jan, 8k XC, Dundalk
+            </p>
+          </div>
+          <div class="mdl-card__menu">
+            <i class="material-icons">play_arrow</i>
+          </div>
+        </div>
+        <div class="mdl-card mdl-cell  mdl-cell--12-col container mdl-shadow--2dp">
             <div class="mdl-card__title">
-              <h2 class="mdl-card__title-text">Speed from the past month</h2>
+              <h2 class="mdl-card__title-text">Speed</h2>
             </div>
             <canvas id="speed_chart"></canvas>
+            <div id="speed_chart_placeholder"></div>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
     <script async src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js" charset="utf-8"></script>
+    <script src="./public/dep/chart.js.legend.js" charset="utf-8"></script>
       <script>
         <?php
         $jsonurl = "https://www.strava.com/api/v3/athlete/activities?&access_token=f93ddad10f5a77e2c0ca100d073012cd4d117c46";
--- public/script.js
+++ public/script.js
@@ -9,6 +9,34 @@ var maxHeartRates = [];
 var averageSpeeds = [];
 var maxSpeeds = [];
 
+var exerciseTypes = {
+  "Run":{
+    value: 0,
+    color:"#2196F3",
+    highlight: "#1976D2",
+    label: "Run"
+  },
+  "Ride": {
+    value: 0,
+    color: "#46BFBD",
+    highlight: "#5AD3D1",
+    label: "Ride"
+  },
+  "Swim": {
+    value: 0,
+    color: "#FDB45C",
+    highlight: "#FFC870",
+    label: "Swim"
+  },
+  "Walk": {
+    value: 0,
+    color: "#F44336",
+    highlight: "#D32F2F",
+    label: "Walk"
+  }
+}
+
+var runData = [];
 stravaData.forEach(function (data) {
   if(data.average_heartrate  != undefined){
     runDates.push(moment(data.start_date).format('Do MMM YY'));
@@ -18,6 +46,19 @@ stravaData.forEach(function (data) {
 
     averageSpeeds.push(data.average_speed);
     maxSpeeds.push(data.max_speed);
+
+  }
+  exerciseTypes[data.type].value = exerciseTypes[data.type].value + 1;
+
+  if (data.type == "Run") {
+    runData.push({
+      distance:(data.distance *0.001).toFixed(2),
+      time: convertTimeString(data.moving_time),
+      date: moment(data.start_date).format('Do MMM YY'),
+      cadence: data.average_cadence,
+      heartrate: data.average_heartrate
+    });
+
   }
 });
 //Reorder the arrays
@@ -29,6 +70,7 @@ maxHeartRates = maxHeartRates.reverse();
 averageSpeeds = averageSpeeds.reverse();
 maxSpeeds = maxSpeeds.reverse();
 
+console.log(runData);
 // heart rate data
 var hrData = {
   labels: runDates,
@@ -37,7 +79,7 @@ var hrData = {
       label: 'Max Heart Rate',
       fillColor: "#F44336",
       strokeColor: "#D32F2F",
-      pointColor: "#2196F3",
+      pointColor: "#F44336",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
       pointHighlightStroke: "rgba(220,220,220,1)",
@@ -58,6 +100,7 @@ var hrData = {
 
 var hrCtx = document.getElementById("heart_rate_chart").getContext("2d");
 var heartRateChart = new Chart(hrCtx).Line(hrData,{responsive: true,maintainAspectRatio: false});
+legend(document.getElementById('heart_rate_chart_placeholder'), hrData);
 
 var speedData = {
   labels: runDates,
@@ -66,7 +109,7 @@ var speedData = {
       label: 'Max Speed',
       fillColor: "#F44336",
       strokeColor: "#D32F2F",
-      pointColor: "#2196F3",
+      pointColor: "#F44336",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
       pointHighlightStroke: "rgba(220,220,220,1)",
@@ -87,3 +130,40 @@ var speedData = {
 
 var speedCtx = document.getElementById("speed_chart").getContext("2d");
 var speedChart = new Chart(speedCtx).Line(speedData,{responsive: true,maintainAspectRatio: false});
+legend(document.getElementById('speed_chart_placeholder'), getData(exerciseTypes));
+
+var exerciseCTX = document.getElementById("exercise_chart").getContext('2d');
+var exercisePieChart = new Chart(exerciseCTX).Doughnut(getData(exerciseTypes));
+legend(document.getElementById('exercise_chart_placeholder'), getData(exerciseTypes));
+
+populateTable();
+
+function populateTable(){
+    var table = document.getElementById('running_table');
+    runData.splice(0,4).forEach(function (data) {
+      var row = table.insertRow(-1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      cell1.innerHTML = data.distance;
+      cell2.innerHTML = data.time;
+      cell3.innerHTML = data.date;
+      cell4.innerHTML = data.cadence;
+      cell5.innerHTML = data.heartrate;
+    });
+}
+
+function getData(exerciseTypes) {
+  var result = [];
+  Object.keys(exerciseTypes).forEach(function (data) {
+    result.push(exerciseTypes[data]);
+  });
+  return result;
+}
+
+function convertTimeString(input) {
+  var time = Math.floor(input/60) + ((input%60)/100);
+  return time.toString().replace(".",":")
+}
